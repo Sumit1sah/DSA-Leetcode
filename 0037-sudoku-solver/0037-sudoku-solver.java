0156d1683f7acca685eac7ac27c8ccc754a3e07c
Sumit Sah <147224454+Sumit1sah@users.noreply.github.com>
2026-09-07 00:08:00 +0530

    Time: 247 ms (48.01%), Space: 47 MB (5.05%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,93 @@
+class Solution {
+
+    static boolean isSafeToPlace(char board[][], char charValue,int rowIndex, int colIndex) {
+
+        // Check row
+        for (int col = 0; col < 9; col++) {
+            if (board[rowIndex][col] == charValue) {
+                return false;
+            }
+        }
+
+        // Check column
+        for (int row = 0; row < 9; row++) {
+            if (board[row][colIndex] == charValue) {
+                return false;
+            }
+        }
+
+        // Check 3 x 3 box
+        int startRow = rowIndex - rowIndex % 3;
+        int startCol = colIndex - colIndex % 3;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                int actualRow = startRow + i;
+                int actualCol = startCol + j;
+
+                if (board[actualRow][actualCol] == charValue) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    static boolean findEmptyCell(char board[][], int emptyCell[]) {
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                if (board[i][j] == '.') {
+                    emptyCell[0] = i;
+                    emptyCell[1] = j;
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    static boolean solveSudokuHelper(char board[][]) {
+
+        int emptyCell[] = new int[2];
+
+        // No empty cell means Sudoku is solved
+        if (!findEmptyCell(board, emptyCell)) {
+            return true;
+        }
+
+        int rowIndex = emptyCell[0];
+        int colIndex = emptyCell[1];
+
+        // Try 1 to 9
+        for (int value = 1; value <= 9; value++) {
+
+            char charValue = (char) (value + '0');
+
+            if (isSafeToPlace(board, charValue, rowIndex, colIndex)) {
+
+                // Place number
+                board[rowIndex][colIndex] = charValue;
+
+                // Recursively solve
+                if (solveSudokuHelper(board)) {
+                    return true;
+                }
+
+                // Backtrack
+                board[rowIndex][colIndex] = '.';
+            }
+        }
+
+        return false;
+    }
+
+    public void solveSudoku(char[][] board) {
+        solveSudokuHelper(board);
+    }
+}
+
