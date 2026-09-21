167900ac695b4d90b51f5ed096cb6a3d9b6e3173
Sumit Sah <147224454+Sumit1sah@users.noreply.github.com>
2026-09-21 15:25:27 +0530

    Time: 0 ms (100%), Space: 42.8 MB (71.54%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -9,16 +9,22 @@
  * }
  */
 class Solution {
+    static ListNode solve(ListNode fast,ListNode slow){
+        if(fast==null){
+            return slow;
+        }
+        fast=fast.next;
+        if(fast!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        ListNode ans=solve(fast,slow);
+        return ans;
+    }
     public ListNode middleNode(ListNode head) {
         ListNode fast=head;
         ListNode slow=head;
-        while(fast!=null){
-            fast=fast.next;
-            if(fast!=null){
-                fast=fast.next;
-                slow=slow.next;
-            }
-        }
-        return slow;
+        ListNode ans=solve(fast,slow);
+        return ans;
     }
 }
